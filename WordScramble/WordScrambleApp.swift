22088becaf67380cef86ca51c0ffa7aa3a23dadd
Scott Obara <scott@rubbscreations.com>
2021-01-13 08:02:17 +1100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Scott Obara on 13/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
