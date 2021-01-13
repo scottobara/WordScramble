e6f5c417fc851f1005f909d0f2e7cfae2180a45b
Scott Obara <scott@rubbscreations.com>
2021-01-13 21:07:31 +1100

    Working with strings: .trimmingCharacters() UITextChecker()

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
     
     var body: some View {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            // we found the file in our bundle!
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // We loaded the file into a string
-            }
-        }
+        let input = """
+                    a
+                    b
+                    c
+                    """
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        return Text("Hello")
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        
+        
+        Text("Hello")
     }
 }
 
@@ -27,3 +35,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
